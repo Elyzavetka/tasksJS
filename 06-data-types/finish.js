a7653f485ab93e5449e55efc9015a8756d43a81e
Elyzavetka <lizamybella@gmail.com>
2023-02-08 21:29:41 +0000

    Update finish.js

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,19 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const myString = "abc";
+const myNumber = 10;
+const myBoolean = true;
+const myNull = null;
+let myUndefinde;
+const myObject = {};
+const myArray = [];
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefinde);
+console.log(typeof myObject);
+console.log(typeof myArray);
