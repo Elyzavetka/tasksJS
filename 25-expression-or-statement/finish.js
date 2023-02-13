590d20499dadefdb338064130a560ea66487d456
Elyzavetka <lizamybella@gmail.com>
2023-02-13 16:10:21 +0000

    Update finish.js

--- 25-expression-or-statement/finish.js
+++ 25-expression-or-statement/finish.js
@@ -6,23 +6,24 @@
  *  - выражение-инструкция (expression statement)
  */
 
-15
+15; // expression
 
+// statement
 const myObject = {
   x: 10,
   y: true,
-}
+};
 
-myObject.z = 'abc'
+myObject.z = "abc"; // expression
 
-delete myObject.x
+delete myObject.x; // statement
 
-let newVariable
+let newVariable; // statement
 
-newVariable = 30 + 5
+newVariable = 30 + 5; // expression
 
-console.log(newVariable)
+console.log(newVariable); // statement
 
 if (newVariable > 10) {
-  console.log(`${newVariable} больше 10`)
+  console.log(`${newVariable} больше 10`); // statement
 }
