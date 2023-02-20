2b5cd0780d1a12db9acb84faa44dc501bf0d8531
Elyzavetka <lizamybella@gmail.com>
2023-02-20 13:22:29 +0000

    Update finish.js

--- 42-is-array-sorted-or-not/finish.js
+++ 42-is-array-sorted-or-not/finish.js
@@ -11,15 +11,38 @@
  * 5. Если массив не отсортирован - вернуть "Массив не отсортирован"
  */
 
-const a = [5, 'abc', 10, 1]
-const b = [4, 10, 14, 25, 25, 50]
-const c = [150, 132, 80, 40]
-const d = [15, 26, 10, 23, 85]
+const arraySortInfo = (inputArray) => {
+  if (inputArray.some((element) => typeof element !== "number")) {
+    return "Some elements are not a numbers";
+  }
+  if (
+    inputArray.every((element, index) =>
+      index > 0 ? element >= inputArray[index - 1] : true
+    )
+  ) {
+    return "Array sorted in ascending order";
+  }
 
-console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
-console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
-console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
-console.log(arraySortInfo(d)) // Массив не отсортирован
+  if (
+    inputArray.every((element, index) =>
+      index > 0 ? element <= inputArray[index - 1] : true
+    )
+  ) {
+    return "array sorted in descending order";
+  }
+
+  return "Array is not sorted";
+};
+
+const a = [5, "abc", 10, 1];
+const b = [4, 10, 14, 25, 25, 50];
+const c = [150, 132, 80, 40];
+const d = [15, 26, 10, 23, 85];
+
+console.log(arraySortInfo(a)); // Некоторые элементы не являются числами
+console.log(arraySortInfo(b)); // Массив отсортирован по возрастанию
+console.log(arraySortInfo(c)); // Массив отсортирован по убыванию
+console.log(arraySortInfo(d)); // Массив не отсортирован
 
 /**
  * ПОДСКАЗКИ
