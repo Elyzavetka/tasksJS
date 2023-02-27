d5366bd36b4b41539a411091ce3f9689a19e0577
Elyzavetka <lizamybella@gmail.com>
2023-02-27 21:39:47 +0000

    Update finish.js

--- 53-enhanced-object-literals/finish.js
+++ 53-enhanced-object-literals/finish.js
@@ -14,19 +14,19 @@
 
 const photosGallery = (title, dimensions, date) => {
   return {
-    title: title,
-    info: function () {
-      console.log(`Фото "${title}" имеет разрешение ${date}`);
+    title,
+    info() {
+      console.log(`Фото "${title}" имеет разрешение ${dimensions}`);
     },
-    dimensions: dimensions,
-    publishInfo: () => {
+    [dimensions]: true,
+    publishInfo() {
       console.log(
-        `Фото было опубликовано ${Math.floor(
+        `Фото "${title}" было опубликовано ${Math.floor(
           (new Date().getTime() - date.getTime()) / 1000
         )} секунды назад`
       );
     },
-    date: date,
+    date,
   };
 };
 
