4b7bdb444daeef423f4db63512514c8c17ebef5a
Elyzavetka <lizamybella@gmail.com>
2023-02-27 15:08:24 +0000

    Update finish.js

--- 51-rest-and-spread-operators/finish.js
+++ 51-rest-and-spread-operators/finish.js
@@ -13,6 +13,11 @@
  * так и оператор "spread".
  */
 function meanScore(...numbers) {
+  if (numbers.some((num) => typeof num !== "number")) {
+    console.error("All arguments should be a numbers");
+    return;
+  }
+
   return numbers
     .reduce((mean, num) => mean + num / numbers.length, 0)
     .toFixed(2);
@@ -23,16 +28,14 @@ const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
 const scores3 = [1.3, 2.5, 1.9];
 const scores4 = ["abc", 1.3, true, 2.5, 1.9];
 
-console.log(meanScore(/* все элементы из массива "scores1" */));
+console.log(meanScore(...scores1));
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */));
+console.log(meanScore(...scores1, ...scores2));
 // 2.8
 
-console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
-);
+console.log(meanScore(...scores1, ...scores2, ...scores3));
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */));
+console.log(meanScore(...scores4));
 // Все аргументы в вызове функции должны быть числами!
