cc9a986726eb667df6e55e55658d0e05b603065b
Elyzavetka <lizamybella@gmail.com>
2023-03-04 17:24:13 +0000

    Created class CustomArray and method customPush.

--- 60-custom-push-method-for-arrays/finish.js
+++ 60-custom-push-method-for-arrays/finish.js
@@ -19,3 +19,17 @@
  * в классе "CustomArray" также будет "push" вместо "customPush"?
  * Попробуйте это.
  */
+
+class CustomArray extends Array {
+  customPush(newElement) {
+    console.log(this.length);
+    this[this.length] = newElement;
+    console.log(this.length);
+    console.log(`New element ${newElement} was just added to array`);
+  }
+}
+
+const myCustomArray = new CustomArray(10, 3, 7, 5);
+myCustomArray.customPush(25);
+
+console.log(myCustomArray);
