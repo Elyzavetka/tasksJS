507aee8544fac57cf0a90f0a6fbfef5f4748a17b
Elyzavetka <lizamybella@gmail.com>
2023-03-13 22:51:33 +0000

    Update finish.js

--- 75-iterate-over-string/finish.js
+++ 75-iterate-over-string/finish.js
@@ -4,12 +4,16 @@
  * Гласные буквы - a, e, i, o, u
  */
 
-let vowelsCount = 0
-const vowels = ['a', 'e', 'i', 'o', 'u']
+let vowelsCount = 0;
+const vowels = ["a", "e", "i", "o", "u"];
 
-const str = 'Today is the best day of my life'
+const str = "Today is the best day of my life";
 
-// Напишите код здесь
+str.split("").forEach((char) => {
+  if (vowels.includes(char)) {
+    vowelsCount += 1;
+  }
+});
 
-console.log(vowelsCount)
+console.log(vowelsCount);
 // 9
